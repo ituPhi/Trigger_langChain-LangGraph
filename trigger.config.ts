a4724a89e34b5e096dfa1d01cdd0acb95530adc0
i2phi <jfiturbe@gmail.com>
2025-06-06 09:40:02 -0500

    set project id from env and add readme

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
-  project: "proj_qibmqhtofjaryowyrucp",
+  project: process.env.PROJECT_ID as string,
   runtime: "node",
   logLevel: "log",
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
